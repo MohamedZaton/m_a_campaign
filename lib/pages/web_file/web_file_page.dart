e6b1c7b1b48f32af0e1ec572df66b486a933a7fd
mohamedzaytoun <mohamed.hassan.1993y@gmail.com>
2022-01-31 01:06:01 +0200

    try find error android 7

--- lib/pages/web_file/web_file_page.dart
+++ lib/pages/web_file/web_file_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
@@ -28,10 +29,8 @@ class _WebFilePageState extends State<WebFilePage> {
 
   TextEditingController emailCtr = TextEditingController();
 
-
   @override
   void initState() {
-
     if (Platform.isAndroid) WebView.platform = AndroidWebView();
   }
 
@@ -48,136 +47,191 @@ class _WebFilePageState extends State<WebFilePage> {
       backgroundColor: kBackGroundColor,
       appBar: AppBar(
         title: Center(child: Text(title)),
-
         actions: [
           /// share button
           InkWell(
-              onTap: () async {
+            onTap: () async {
+              Get.bottomSheet(Container(
+                color: Colors.white,
+                width: ScreenMobile.width(context),
+                height: ScreenMobile.heigth(context) * 0.3,
+                child: Form(
+                  key: _formSharetKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(1.8),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: ScreenMobile.width(context) * 0.02,
+                        ),
 
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "E-mail :",
+                              style: kTitleTextStyle,
+                            ),
+                          ],
+                        ),
 
-                Get.bottomSheet(Container(
-                  color: Colors.white,
-                  width: ScreenMobile.width(context),
-                  height: ScreenMobile.heigth(context) * 0.3,
-                  child: Form(
-                    key: _formSharetKey,
-                    child: Padding(
-                      padding: const EdgeInsets.all(1.8),
+                        const Text(
+                          "Please write your email address to received this website",
+                          style: kDetailsTxtStyle,
+                        ),
+                        SizedBox(
+                          height: ScreenMobile.width(context) * 0.02,
+                        ),
+
+                        /// email field
+                        TextFormField(
+                          controller: emailCtr,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            hintText: kEmailTxt,
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'This field is required ';
+                            } else if (!GetUtils.isEmail(value)) {
+                              return 'Please enter a valid E-mail Address';
+                            } else {}
+                          },
+                        ),
+                        SizedBox(
+                          height: ScreenMobile.width(context) * 0.05,
+                        ),
+
+                        /// Register Form
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formSharetKey.currentState!.validate()) {
+                              print("valid Form True");
+                              // SendMailsUtil.sendSmtpGmail(
+                              //     emailCtr.text, webfileLogic.htmlPath.value);
+                              SendMailsUtil.sendShareNotification(
+                                  emailCtr.text, webfileLogic.htmlPath.value);
+                              Get.back();
+                              Get.snackbar(
+                                "Success ",
+                                "Please check your email",
+                                icon: Icon(
+                                  Icons.check_circle,
+                                  color: Colors.green,
+                                ),
+                              );
+                            } else {
+                              print("vaild F FF");
+                              Get.back();
+                            }
+                          },
+                          child: Text(
+                            kSendTxt,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: kLightPrimary,
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(8), // <-- Radius
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ));
+            },
+            child: Icon(Icons.share_outlined),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          // qr button
+          !isHtmlLocal
+              ? InkWell(
+                  onTap: () async {
+                    Get.bottomSheet(Container(
+                      color: Colors.white,
+                      width: ScreenMobile.width(context),
+                      height: ScreenMobile.heigth(context) * 0.5,
                       child: Column(
                         children: [
                           SizedBox(
-                            height: ScreenMobile.width(context) * 0.02,
+                            height: 2,
                           ),
-
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: const [
-
-                              Text(
-                                "E-mail :",
-                                style: kTitleTextStyle,
-                              ),
-                            ],
-                          ),
-
-                          const Text(
-                            "Please write your email address to received this website",
-                            style: kDetailsTxtStyle,
+                          QrImage(
+                            data: webfileLogic.htmlPath.value,
+                            version: QrVersions.auto,
+                            size: 200.0,
                           ),
                           SizedBox(
-                            height: ScreenMobile.width(context) * 0.02,
+                            height: 4,
                           ),
-
-                          /// email field
-                          TextFormField(
-                            controller: emailCtr,
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: const InputDecoration(
-                              hintText: kEmailTxt,
-                              fillColor: Colors.white,
-                              filled: true,
-                              border: OutlineInputBorder(),
-                            ),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'This field is required ';
-                              } else if (!GetUtils.isEmail(value)) {
-                                return 'Please enter a valid E-mail Address';
-                              } else {}
-                            },
+                          Text(
+                            "Scan ",
+                            style: TextStyle(
+                                color: kLightAccent,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15),
                           ),
-                          SizedBox(
-                            height: ScreenMobile.width(context) * 0.05,
+                          Wrap(children: [
+                            Text(
+                              "The QR Code to visit  Website ",
+                              style: kTitleTextStyle.copyWith(fontSize: 10),
+                            )
+                          ]),
+                          Text(
+                            "Steps : ",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: kLightAccent,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
                           ),
-
-                          /// Register Form
-                          ElevatedButton(
-                            onPressed: () {
-                              if (_formSharetKey.currentState!.validate()) {
-                                print("valid Form True");
-                                // SendMailsUtil.sendSmtpGmail(
-                                //     emailCtr.text, webfileLogic.htmlPath.value);
-                                SendMailsUtil.sendShareNotification(
-                                    emailCtr.text, webfileLogic.htmlPath.value);
-                                Get.back();
-                                Get.snackbar("Success ", "Please check your email",icon: Icon(Icons.check_circle,color: Colors.green,),);
-
-                              } else {
-                                print("vaild F FF");
-                                Get.back();
-                              }
-                            },
-                            child: Text(
-                              kSendTxt,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
+                          Text(
+                            "1. Open the QR Code reader or the camera on your phone.",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: kDarkGray,
+                              fontSize: 10,
                             ),
-                            style: ElevatedButton.styleFrom(
-                              primary: kLightPrimary,
-                              shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.circular(8), // <-- Radius
-                              ),
+                          ),
+                          Text(
+                            "2. Then point your  phone at the QR code to scan it.",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: kDarkGray,
+                              fontSize: 10,
                             ),
-                          )
+                          ),
+                          Text(
+                            "3. Finally, tap the pop-up banner or button.",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: kDarkGray,
+                              fontSize: 10,
+                            ),
+                          ),
                         ],
                       ),
-                    ),
-                  ),
-                ));
-              },
-              child: Icon(Icons.share_outlined),),
-          SizedBox(width: 10,),
-          // qr button
-          !isHtmlLocal?InkWell(
-
-              onTap: () async {
-                Get.bottomSheet(Container(
-                  color: Colors.white,
-                  width: ScreenMobile.width(context),
-                  height: ScreenMobile.heigth(context) * 0.5,
-                  child:Column(
-                    children: [
-                      SizedBox(height: 2,),
-                      QrImage(
-                        data: webfileLogic.htmlPath.value,
-                        version: QrVersions.auto,
-                        size: 200.0,
-                      ),
-                      SizedBox(height: 4,),
-                      Text("Scan ",style: TextStyle(color: kLightAccent , fontWeight: FontWeight.bold,fontSize: 15 ),),
-                      Wrap(children: [Text("The QR Code to visit  Website ",style: kTitleTextStyle.copyWith(fontSize:10 ),)]),
-                      Text("Steps : ",textAlign: TextAlign.start,style: TextStyle(color: kLightAccent , fontWeight: FontWeight.bold,fontSize: 15, ),),
-                      Text("1. Open the QR Code reader or the camera on your phone.",textAlign: TextAlign.start,style: TextStyle(color: kDarkGray ,fontSize: 10, ),),
-                      Text("2. Then point your  phone at the QR code to scan it.",textAlign: TextAlign.start,style: TextStyle(color: kDarkGray ,fontSize: 10, ),),
-                      Text("3. Finally, tap the pop-up banner or button.",textAlign: TextAlign.start,style: TextStyle(color: kDarkGray ,fontSize: 10, ),),
-                    ],
-                  ),
-                ));
-              },
-              child: Icon(Icons.qr_code),):SizedBox(width: 4,),
-          SizedBox(width: 10,)
+                    ));
+                  },
+                  child: Icon(Icons.qr_code),
+                )
+              : SizedBox(
+                  width: 4,
+                ),
+          SizedBox(
+            width: 10,
+          )
         ],
       ),
       // webView controll
@@ -199,6 +253,15 @@ class _WebFilePageState extends State<WebFilePage> {
                     onPageFinished: (url) {
                       webfileLogic.isLoading.value = false;
                     },
+                    onProgress: (progress) {
+                      webfileLogic.isLoading.value = true;
+                    },
+                    onWebResourceError: (error) {
+                      print("[zt] Webview error type : ${error.errorType}");
+                      print("[zt] Webview error code : ${error.errorCode}");
+                      print(
+                          "[zt] Webview error description : ${error.description}");
+                    },
                     javascriptMode: JavascriptMode.unrestricted,
                   ),
                   webfileLogic.isLoading.value
@@ -211,5 +274,4 @@ class _WebFilePageState extends State<WebFilePage> {
             }),
     );
   }
-
 }
